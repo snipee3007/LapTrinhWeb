7700b2d44a4209aa25e9cae91e8849101bc687c0
FazeCT <110371121+FazeCT@users.noreply.github.com>
2024-05-02 10:22:46 +0700

    Add files via upload

---
+++ client/src/components/header.php
@@ -0,0 +1,78 @@
+<header x-data="{open: false}" @keydown.window.escape="open = false" class="bg-white">
+<nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+    <div class="flex lg:flex-1">
+    <a href="#" class="-m-1.5 p-1.5">
+        <span class="sr-only">CoCha Gear</span>
+        <img class="h-8 w-auto" src="https://cdn.haitrieu.com/wp-content/uploads/2021/09/Logo-DH-Bach-Khoa-HCMUT.png" alt="Logo">
+    </a>
+    </div>
+
+    <div class="flex lg:hidden">
+    <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700" @click="open = true">
+        <span class="sr-only">Mở menu</span>
+        <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentcolor" aria-hidden="true">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+        </svg>
+    </button>
+    </div>
+
+    <div class="hidden lg:flex lg:gap-x-12">
+    <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Giới thiệu</a>
+    <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Tin tức</a>
+    <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Hệ thống cửa hàng</a>
+    <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Liên hệ</a>
+    </div>
+    <div class="hidden lg:flex lg:gap-x-12 lg:flex-1 lg:justify-end">
+    <a href="#" class="text-sm font-semibold leading-6 text-gray-900 flex items-center gap-x-1">
+        <span>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentcolor" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+        </svg>
+        </span>
+        Đăng nhập
+    </a>
+
+    <a href="#" class="text-sm font-semibold leading-6 text-gray-900 flex items-center gap-x-1">
+        <span>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentcolor" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+        </svg>
+        </span>
+        Giỏ hàng
+    </a>
+    </div>
+</nav>
+
+<div class="lg:hidden" role="dialog" aria-modal="true" x-show="open">
+    <div class="fixed inset-0 z-10"></div>
+    <div class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10" @click.away="open = false">
+    <div class="flex items-center justify-between">
+        <a href="#" class="-m-1.5 p-1.5">
+        <span class="sr-only">CoCha Gear</span>
+        <img class="h-8 w-auto" src="https://cdn.haitrieu.com/wp-content/uploads/2021/09/Logo-DH-Bach-Khoa-HCMUT.png" alt="Logo">
+        </a>
+        <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" @click="open=false">
+        <span class="sr-only">Đóng menu</span>
+        <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentcolor" aria-hidden="true">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+        </svg>
+        </button>
+
+    </div>
+    <div class="mt-6 flow-root">
+        <div class="-my-6 divide-y divide-gray-500/10">
+        <div class="space-y-2 py-6">
+            <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Giới thiệu</a>
+            <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Tin tức</a>
+            <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Hệ thống cửa hàng</a>
+            <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Liên hệ</a>
+        </div>
+        <div class="py-6">
+            <a href="#" class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Đăng nhập</a>
+            <a href="#" class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Giỏ hàng</a>
+        </div>
+        </div>
+    </div>
+    </div>
+</div>
+</header>
